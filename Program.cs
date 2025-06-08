147ee007e31af234615dbf9b4eb619205a92df2c
Shamik200 <119531911+Shamik200@users.noreply.github.com>
2025-06-08 11:11:25 +0530

    Downgrade target to net7.0 for Render

--- Program.cs
+++ Program.cs
@@ -1,15 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    // Optional: add any development-only middleware here
 }
 
 app.UseHttpsRedirection();
@@ -21,7 +20,7 @@ var summaries = new[]
 
 app.MapGet("/weatherforecast", () =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
+    var forecast = Enumerable.Range(1, 5).Select(index =>
         new WeatherForecast
         (
             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
